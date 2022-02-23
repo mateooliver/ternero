d76fcffcb98ad7517c446c7848c0ff1b0450c3a3
Mateo Oliver <mateooliver@MacBook-Air-de-Mateo.local>
2022-02-22 21:49:32 -0300

    reparacion del segundo redireccion

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@ function enviarDatos() {
     sessionStorage.setItem("data", mensaje)
     // le configuro un retardo para que se pueda leer el anuncio de que los datos se cargaron bien
     function tardanza(){ 
-         window.location.href="login.html"}
+         window.location.href="/ternero/pages/login.html"}
 
     setTimeout  (tardanza,1200);
     }
